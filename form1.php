43e016e6ac7ec9ce090159786e31db0b5b0ad407
Ruben221510058 <132134071+Ruben221510058@users.noreply.github.com>
2023-04-29 16:32:23 +0700

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Formulir</title>
+</head>
+
+<body>
+    <form action="proses1.php" method="POST">
+        Nama Lengkap : <br>
+        <input type="text" name="txtnama"><br>
+        Jenis Kelamin: <br>
+        <input type="radio" name="jenkel" value="Laki-laki">Laki-laki
+        <input type="radio" name="jenkel" value="Perempuan">Perempuan <br>
+        <button type="submit">Proses</button>
+</form>
+</body>
+</html>
---
+++ kirim.php
@@ -0,0 +1,7 @@
+<html> 
+ <head></head> 
+	<body>
+	<a href="baca.php?nama=Ruben">Klik</a>
+	<a href="baca2.php?nama=Putri&jenkel=lakilaki">Klik2</a>
+	</body> 
+    </html>
